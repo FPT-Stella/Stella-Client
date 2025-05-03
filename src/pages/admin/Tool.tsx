ed33e563e22ff6b7bd2979f61a2a07b9b3b471e6
TAT610 <trananhtuyet0610@gmail.com>
2025-05-04 04:33:57 +0700

    1

--- src/pages/admin/Tool.tsx
+++ src/pages/admin/Tool.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Tool() {
   return <div>Tool</div>;
 }
---
+++ src/services/Tool.ts
@@ -0,0 +1,41 @@
+
+import rootApi from "./rootApi";
+import { Tool,CreateTool } from "../models/Tool";
+
+export const getTool = async()=>{
+    try{
+        const response = await rootApi.get("/Tool");
+        return response.data;
+    } catch(error){
+        console.error("Error fetching Tool", error);
+        throw error;
+    }
+}
+
+export const deleteTool = async (id: string) => {
+    try {
+      const response = await rootApi.delete(`/Tool/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error deleting Tool:", error);
+      throw error;
+    }
+  };
+  export const addTool = async (data:CreateTool) => {
+    try {
+      const response = await rootApi.post("/Tool", data);
+      return response.data;
+    } catch (error) {
+      console.error("Error adding program:", error);
+      throw error;
+    }
+  };
+  export const updateProgram = async (id: string, data: Partial<Tool>) => {
+      try {
+        const response = await rootApi.put(`/Tool/${id}`, data);
+        return response.data;
+      } catch (error) {
+        console.error(`Error updating Tool with ID ${id}:`, error);
+        throw error;
+      }
+    };
