cc9a998e1a36b9704eba31ff241278aaa4014d20
TAT610 <trananhtuyet0610@gmail.com>
2025-05-04 06:11:13 +0700

    Done Tool

--- src/models/Tool.tsx
+++ src/models/Tool.tsx
@@ -2,10 +2,10 @@ export interface Tool {
   toolName: string;
   description: string;
   id: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 export interface CreateTool {
   toolName: string;
   description: string;
-  createdAt: string;
-  updatedAt: string;
 }
